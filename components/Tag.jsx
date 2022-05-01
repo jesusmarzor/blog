5f9797c5c086e2acb856748368d8e3b6caa89eb9
Jesus Martin <jesusmarzor28@gmail.com>
2022-05-01 17:26:18 +0200

    Actualizar tags seleccionados

--- components/Tag.jsx
+++ components/Tag.jsx
@@ -1,13 +1,15 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export function Tag({children, tag}){
+    const router = useRouter();
     return (
         <Link href={(tag === 'all') ? '/' : `/tags/${tag}`}>
-            <a className="flex justify-center items-center px-3 py-2 mx-2 mb-2 rounded-3xl border-2 border-gray cursor-pointer hover:border-sky-500 transition duration-300 ease">
+            <a className={`flex justify-center items-center px-3 py-2 mx-2 mb-2 rounded-3xl border-2 border-gray cursor-pointer hover:border-sky-500 transition duration-300 ease ${(tag === 'all' && !router.query.tag) && 'border-sky-500'} ${(router.query.tag === tag) && 'border-sky-500'}`}>
                 {
                     (tag !== 'all')
-                    ?
+                    &&
                         <div className="flex justify-centr items-center mr-2">
                             <Image
                                 src={`/img/${tag}.png`}
@@ -16,8 +18,6 @@ export function Tag({children, tag}){
                                 height={25}
                             />
                         </div>
-                    :
-                        null
                 }
                 {children}
             </a>
